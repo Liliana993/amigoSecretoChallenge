6e6070a428ba8a05fd70002e3e016d56f35be4f8
Liliana993 <lili_esco@yahoo.com.ar>
2025-01-22 12:03:44 -0300

    función agregar amigo creada

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+//Creo el array vacío para almacenar los amigos
+let amigos = [];
+let amigo;
+
+function agregarAmigo(){
+    //tomo el valor del input
+    const amigo = document.querySelector('#amigo').value;
+    if( amigo == null || amigo.length == 0 || /^\s+$/.test(amigo )){
+        alert('Por favor introduce un nombre válido para continuar con el juego.')
+        return false
+    }else{
+        //agrego los nombres dentro del array
+        amigos.push(amigo);
+    //Recorremos nuestro array con foreach
+        amigos.forEach((amigo)=>{
+            console.log(amigo);
+        })
+    }
+}
